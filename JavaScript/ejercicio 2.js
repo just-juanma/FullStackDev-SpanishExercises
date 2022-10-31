101fbb39767e94d9581db7b44422382e7ba4bea8
Juan Manuel <jugoka947@gmail.com>
2022-10-31 13:23:05 -0300

    ex2

---
+++ JavaScript/ejercicio 2.js
@@ -0,0 +1,26 @@
+/* 
+
+Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
+- Tu nombre (string)
+
+- Tu edad (number)
+
+- ¿Eres desarrollador? (boolean)
+
+- Tu fecha de nacimiento (Date)
+
+- Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+
+*/
+
+const libro = {
+    titulo: "El sabueso de los Baskerville",
+    autor: "Arthur Conan Doyle",
+    fecha: new Date(1901, 4, 5),
+    url: "https://es.wikipedia.org/wiki/El_sabueso_de_los_Baskerville"
+}
+
+let nombre = "Juan Manuel";
+let edad = 19;
+let desarrollador = true;
+let date = new Date(2003, 3, 9);
