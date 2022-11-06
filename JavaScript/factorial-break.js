3a289980c9e7527a96e81cd9f6e66f93b07d8fd8
Juan Manuel <jugoka947@gmail.com>
2022-11-06 11:38:09 -0300

    ex

---
+++ JavaScript/factorial-break.js
@@ -0,0 +1,8 @@
+let result = 1;
+let i = 0;
+
+while (i < 999) {
+  if (i == 10) break;
+  result *= i + 1;
+  i++;
+}
---
+++ JavaScript/factorial-while.js
@@ -0,0 +1,7 @@
+let result = 1;
+let i = 0;
+
+while (i < 10) {
+  result *= i + 1;
+  i++;
+}
